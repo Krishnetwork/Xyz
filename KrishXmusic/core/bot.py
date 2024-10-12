7c6ea35f76135419cc80c0b61878d9c07fa96de0
𝐊ʀɪsʜ 𝐌ɪsʜʀᴀ <vimal9559459031@gmail.com>
2024-10-12 08:56:27 +0530

    Update bot.py

--- KrishXmusic/core/bot.py
+++ KrishXmusic/core/bot.py
@@ -6,17 +6,16 @@ import config
 from ..logging import LOGGER
 
 
-class Hotty (Client):
-    def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
-        super().__init__(
-            name="KrishXmusic",
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            bot_token=config.BOT_TOKEN,
-            in_memory=True,
-            max_concurrent_transmissions=7,
-        )
+def __init__(self):
+    LOGGER(__name__).info(f"Starting Bot...")
+    super().__init__(
+        name="KrishXmusic",
+        api_id=config.API_ID,
+        api_hash=config.API_HASH,
+        bot_token=config.BOT_TOKEN,
+        in_memory=True,
+        max_concurrent_transmissions=7,
+    )
 
     async def start(self):
         await super().start()
